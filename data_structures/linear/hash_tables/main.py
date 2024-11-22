a89e6dff0d71900769cc4e491e6ecef3d76c432a
christophergyman <christophergayiuman@gmail.com>
2024-11-22 13:27:02 +0000

    update: updated hashtable

--- data_structures/linear/hash_tables/main.py
+++ data_structures/linear/hash_tables/main.py
@@ -1,5 +1,14 @@
 def hash_table():
-    return -1
+    thisdict = {
+        "brand": "Ford",
+        "model": "Mustang",
+        "year": 1964
+    }
+
+    print(thisdict)
+    print(thisdict["brand"])
+    thisdict = dict(name="John", age=36, country="Norway")
+    print(thisdict)
 
 if __name__ == '__main__':
     hash_table()
