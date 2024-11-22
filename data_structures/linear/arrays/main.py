b99f0a79d2d9bc36aa150812434718578f6c3297
christophergyman <christophergayiuman@gmail.com>
2024-11-22 13:24:24 +0000

    feat: added array

---
+++ data_structures/linear/arrays/main.py
@@ -0,0 +1,9 @@
+def array():
+    my_array = []
+    my_array.append("yoyo")
+    my_array.pop()
+    my_array.append("popper")
+    print(my_array)
+
+if __name__ == '__main__':
+    array()
---
+++ data_structures/linear/hash_tables/main.py
@@ -0,0 +1,5 @@
+def hash_table():
+    return -1
+
+if __name__ == '__main__':
+    hash_table()
